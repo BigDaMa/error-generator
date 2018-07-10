db961309343994f9f1a51770744bf81a3c2e8837
milad-abbbaszadeh <milad.abbaszadeh94@gmail.com>
2018-07-10 15:51:50 +0200

    10 july

--- API_error_generator.py
+++ API_error_generator.py
@@ -1,3 +1,14 @@
+########################################
+# Error Generator
+# Milad Abbaszadeh
+# Milad.abbaszadeh94@gmail.com
+# July 2018
+# Big Data Management Group
+# TU Berlin
+# All Rights Reserved
+########################################
+
+########################################
 import os
 import json
 import re
@@ -6,16 +17,19 @@ import pandas
 import numpy as np
 import psycopg2
 from random import *
-w=choice
-o=ord
 import random
 import butterfingers
 import difflib
 import sys
 dataset_dataframe_version=None
+import chainer, chainer.links as L,chainer.functions as F
+from chainer import Variable
+w=choice
+o=ord
+#######################################################
 
 
-#----------------- read csv file ----------------------------
+#----------------- read & write csv file ----------------------------
 
 def read_csv_dataset(dataset_path, header_exists=True):
     """
@@ -36,6 +50,16 @@ def read_csv_dataset(dataset_path, header_exists=True):
 
         dataset_dataframe = dataset_dataframe.apply(lambda x: x.str.strip())
         return dataset_dataframe.get_values().tolist()
+
+
+def write_csv_dataset(dataset_path, dataset_table):
+    """
+    The method writes a dataset to a csv file path.
+    """
+    dataset_dataframe = pandas.DataFrame(data=dataset_table[1:], columns=dataset_table[0])
+    dataset_dataframe.to_csv(dataset_path, sep=",", header=True, index=False, encoding="utf-8")
+
+
 #-----------------TYPOYS--------------------------------
 
 def typoGenerator(col_name,percentage):
@@ -104,24 +128,33 @@ def explicit_missing_value(col_name,percentage):
     return dataset
 
 
-def implicit_missing_value_mean(col_name,percentage):
+def implicit_missing_value_mean_median_mode(col_name,percentage):
     implicit_missing_value_history = []
     number = int((percentage / 100) * (len(dataset) - 1))
     col = dataset[0].index(col_name)
     mean_value = dataset_dataframe_version[col_name].mean(axis=0)
+    median_value = dataset_dataframe_version[col_name].median(axis=0)
+    mod_value = dataset_dataframe_version[col_name].mode()
 
-    print(mean_value)
-    print("---------Change according to implicit missing value(Mean) method ---------------\n")
+
+    print("---------Change according to implicit missing value(Mean/Median/Mode) method ---------------\n")
     for i in range(number):
         random_value = random.randint(1, len(dataset) - 1)
         while random_value in implicit_missing_value_history:
             random_value = random.randint(1, len(dataset) - 1)
         implicit_missing_value_history.append(random_value)
         temp = dataset[random_value][col]
-        if temp==mean_value:
-            mean_value=mean_value+1
-        dataset[random_value][col] = mean_value
-        print("row: {} col: {} : '{}' changed to {}  ".format(random_value, col, temp,mean_value))
+
+        col_list=[float(mean_value),float(median_value),float(mod_value)]
+        selected=min(col_list, key=lambda x: abs(x - float(temp)))
+
+        while temp==selected:
+            col_list=col_list.remove(selected)
+            selected = min(col_list, key=lambda x: abs(x - float(temp)))
+        if len(col_list)==0:
+            selected=mean_value+1
+        dataset[random_value][col] = selected
+        print("row: {} col: {} : '{}' changed to {}  ".format(random_value, col, temp,selected))
     return dataset
 
 
@@ -211,25 +244,92 @@ def noise(col_name,percentage):
             random_value = random.randint(1, len(dataset) - 1)
         noise_history.append(random_value)
         selected=dataset[random_value][col]
+
         dataset[random_value][col]=float(selected)+noise[0]
 
         print("row: {} col: {} : '{}' changed to '{}'  ".format(random_value,col,selected,dataset[random_value][col]))
     return dataset
 
+def noise_gaussian(col_name,percentage,noise_rate):
+    implicit_missing_value_history = []
+    number = int((percentage / 100) * (len(dataset) - 1))
+    col = dataset[0].index(col_name)
+
+
+    print("---------Change according to gaussian noise method ---------------\n")
+    for i in range(number):
+        random_value = random.randint(1, len(dataset) - 1)
+        while random_value in implicit_missing_value_history:
+            random_value = random.randint(1, len(dataset) - 1)
+        implicit_missing_value_history.append(random_value)
+        temp = dataset[random_value][col]
+        # print(type(temp))
+        if (isinstance(temp, str)):
+
+            asci_str=0
+            for ch in temp:
+                code = ord(ch)
+                asci_str=code+asci_str
+            added_str=chr(asci_str)
+
+            temp_array = np.array([asci_str]).astype(np.float32)
+            # batch,dim = v.data.shape[0],v.data.shape[1]
+            batch = 1
+            ones = Variable(np.ones((batch), dtype=np.float32))
+            new_value = F.gaussian(temp_array, noise_rate * ones)
+
+            if int(new_value.data[0])==asci_str:
+                print("your noise rate is not alot so the strings will reverse as noise")
+                new_value = temp[::-1]
+                replaced_value=new_value
+                dataset[random_value][col] = replaced_value
+            else:
+
+                replaced_value = temp + added_str
+                dataset[random_value][col] = replaced_value
+
+        else:
+            temp_array = np.array([temp]).astype(np.float32)
+            # batch,dim = v.data.shape[0],v.data.shape[1]
+            batch=1
+            ones = Variable(np.ones((batch), dtype=np.float32))
+            new_value=F.gaussian(temp_array, noise_rate * ones)
+            dataset[random_value][col] = new_value.data[0]
+            replaced_value=new_value.data[0]
+
+
+        print("row: {} col: {} : '{}' changed to {}  ".format(random_value, col, temp,replaced_value))
+    return dataset
+
+
+
+
+
 
 if __name__=="__main__":
-    dataset = read_csv_dataset("/home/milad/Desktop/error-generator/test.csv")
 
-    typoGenerator("dept",50)
-    typoGenerator2("dept",50)
-    explicit_missing_value("dept",50)
-    random_active_domain("dept",50)
-    similar_based_active_domain("dept",50)
-    noise("salary",50)
-    implicit_missing_value_mean("salary",50)
+    # dataset = read_csv_dataset("/home/milad/Desktop/error-generator/dataset/test.csv")
+    # typoGenerator("dept",50)
+    # typoGenerator2("dept",50)
+    # explicit_missing_value("dept",50)
+    # random_active_domain("dept",50)
+    # similar_based_active_domain("dept",50)
+    # noise("salary",50)
+    # implicit_missing_value_mean_median_mode("salary",50)
+    # noise_gaussian("salary", 50, -10)
+    # write_csv_dataset("output/out.csv", dataset)
 
+    dataset = read_csv_dataset("dataset/address_10_ground_truth.csv")
+    typoGenerator("FirstName",1)
+    typoGenerator2("City",1)
+    explicit_missing_value("Address",1)
+    implicit_missing_value_mean_median_mode("ZIP", 1)
+    random_active_domain("ZIP",1)
+    similar_based_active_domain("City",1)
+    noise("ZIP",1)
+    noise_gaussian("State",1,10)
 
-
+    write_csv_dataset("output/out.csv",dataset)
 
 
 
--- test.py
+++ test.py
@@ -3,10 +3,10 @@ import numpy as np
 import csv
 import pandas
 import random
-
 dataset_dataframe_version=None
 import chainer, chainer.links as L,chainer.functions as F
 from chainer import Variable
+
 
 
 def read_csv_dataset(dataset_path, header_exists=True):
@@ -31,39 +31,46 @@ def read_csv_dataset(dataset_path, header_exists=True):
 
 
 
-
-
-def noise_gaussian(col_name,percentage,noise_rate):
+def implicit_missing_value_mean_median_mode(col_name,percentage):
     implicit_missing_value_history = []
     number = int((percentage / 100) * (len(dataset) - 1))
     col = dataset[0].index(col_name)
+    mean_value = dataset_dataframe_version[col_name].mean(axis=0)
+    median_value = dataset_dataframe_version[col_name].median(axis=0)
+    mod_value = dataset_dataframe_version[col_name].mode()
 
 
-    print("---------Change according to gaussian noise method ---------------\n")
+    print("---------Change according to implicit missing value(Mean/Median/Mode) method ---------------\n")
     for i in range(number):
         random_value = random.randint(1, len(dataset) - 1)
         while random_value in implicit_missing_value_history:
             random_value = random.randint(1, len(dataset) - 1)
         implicit_missing_value_history.append(random_value)
         temp = dataset[random_value][col]
-        temp_array = np.array([temp]).astype(np.float32)
-        # batch,dim = v.data.shape[0],v.data.shape[1]
-        batch=1
-        ones = Variable(np.ones((batch), dtype=np.float32))
-        new_value=F.gaussian(temp_array, noise_rate * ones)
 
+        col_list=[float(mean_value),float(median_value),float(mod_value)]
+        selected=min(col_list, key=lambda x: abs(x - float(temp)))
 
-        dataset[random_value][col] = new_value
-        print("row: {} col: {} : '{}' changed to {}  ".format(random_value, col, temp,new_value))
+        while temp==selected:
+            col_list=col_list.remove(selected)
+            selected = min(col_list, key=lambda x: abs(x - float(temp)))
+        if len(col_list)==0:
+            selected=mean_value+1
+        dataset[random_value][col] = selected
+        print("row: {} col: {} : '{}' changed to {}  ".format(random_value, col, temp,selected))
     return dataset
+
+
+
+
+
 
 
 
 
 if __name__=="__main__":
 
-    dataset = read_csv_dataset("/home/milad/Desktop/error-generator/test.csv")
-    noise_gaussian("salary",50,-10)
-    print(dataset)
-
-
+    dataset = read_csv_dataset("/home/milad/Desktop/error-generator/dataset/hosp_holoclean.csv")
+    # implicit_missing_value_mean_median_mode("salary",50)
+    mean_value = dataset_dataframe_version["zip"].mean(axis=0)
+    print(mean_value)
