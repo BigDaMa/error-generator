c2444dfd8eb4224710e98285080421f5935b2873
milad-abbbaszadeh <milad.abbaszadeh94@gmail.com>
2018-11-14 00:21:57 +0100

    remove

--- error_generator/strategies/missing_value/explicit_missing_value/explicit_missing_value.py
+++
@@ -1,9 +0,0 @@
-
-class Explicit_Missing_Value(object):
-    def __init__(self,name="Explicit_Missing_Value"):
-        self.name=name
-
-    def run(self,row,col,selected_value,dataset):
-        temp=""
-        return temp
-
